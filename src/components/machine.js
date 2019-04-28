bfaed0f25bf595e9fd66d2625bf379ee0992c706
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-04-27 23:31:50 -0300

    machine image

--- src/components/machine.js
+++ src/components/machine.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './machine.css'
 
-export default function Machine() {
+export default function Machine({ image }) {
   return (
     <div className="machine">
       I'm a machine
