31c0e523ccc328bb963e3c07f5b7420dbb67782e
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-05-07 14:16:02 -0300

    machines selector

--- src/components/machines/machines.js
+++ src/components/machines/machines.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import seller from '../../images/seller.png'
-import crafter from '../../images/crafter.png'
-import furnace from '../../images/furnace.png'
-import starter from '../../images/starter.png'
-import transporter from '../../images/transporter.png'
+import machines from '../../data/machinesSelector'
 
 import './machines.css'
 
@@ -11,11 +7,7 @@ export default function Machines() {
   return (
     <div className="machines">
 
-      <img className="machine" src={starter} alt='1' />
-      <img className="machine" src={seller} alt='1' />
-      <img className="machine" src={furnace} alt='1' />
-      <img className="machine" src={crafter} alt='1' />
-      <img className="machine" src={transporter} alt='1' />
+      { machines.map(({ className, src, alt }, i) => <img key={i} className={className} src={src} alt={alt} onClick={() => {}} />) }
 
     </div>
   )
