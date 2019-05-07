786869b5727641c9178ed9cedc39dc77757e64b9
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-05-06 23:27:43 -0300

    panel storybook fix

--- src/stories/panel.storybook.js
+++ src/stories/panel.storybook.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-
+import storeCreator from '../storeCreator'
 import Panel from '../components/panel/panel'
+import { Provider } from 'react-redux'
+
 
 storiesOf('Panel', module)
   .add('Panel', () =>
-    <Panel/>
+    <Provider store={storeCreator}>
+      <Panel />
+    </Provider>
   )
