71f486bd36dfb1f1be30232e0ee306eb9dd96411
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-05-07 22:29:22 -0300

    machine select in panel

--- src/components/machines/machines.js
+++ src/components/machines/machines.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import machines from '../../data/machinesSelector'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import updateMachineSelected from '../../actions/updateMachineSelected'
 
 import './machines.css'
 
-export default function Machines() {
-  return (
-    <div className="machines">
+class MachinesSelector extends Component {
 
-      { machines.map(({ className, src, alt }, i) => <img key={i} className={className} src={src} alt={alt} onClick={() => {}} />) }
+  render() {
+    const { machinesSelector, machineSelected, updateMachineSelected } = this.props;
 
-    </div>
-  )
+    return (
+      <div className="machines">
+
+        { machinesSelector.map(({ className, src, alt }, i) => <img key={i} 
+        className={className} src={src} alt={alt} 
+        onClick={() => updateMachineSelected(i)} />) }
+
+        { machineSelected }
+
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  machinesSelector: state.panel.machinesSelector,
+  machineSelected: state.panel.machineSelected
+});
+
+const mapDispatchToProps = {
+  updateMachineSelected
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MachinesSelector);
--- src/reducers/panel.js
+++ src/reducers/panel.js
@@ -1,15 +1,31 @@
 import { type as updateSelected } from '../actions/updateSelected';
+import { type as updateMachineSelected } from '../actions/updateMachineSelected';
 import machines from '../data/machines';
+import machinesSelector from '../data/machinesSelector';
 
-const initialState = { machines, selected: 24 };
+const initialState = { 
+  machines, 
+  selected: 24, 
+  machinesSelector, 
+  machineSelected: -1
+};
 
 function panel(state = initialState, { type, selected }) {
   switch (type) {
     case updateSelected: {
       const machinesUpdated = state.machines;
       machinesUpdated[state.selected] = Object.assign({}, machinesUpdated[state.selected], { className: 'piece' });
-      machinesUpdated[selected] = Object.assign({}, machinesUpdated[selected], { className: 'selected' });
-      return { machines: machinesUpdated, selected };
+      machinesUpdated[selected] = Object.assign({}, machinesUpdated[selected], 
+        state.machineSelected === -1
+        ? { className: 'selected' }
+        : { className: 'selected', src: state.machinesSelector[state.machineSelected].src });
+      return Object.assign({}, state, { machines: machinesUpdated, selected });
+    }
+    case updateMachineSelected: {
+      const machinesSelectorUpdated = state.machinesSelector;
+      machinesSelectorUpdated[state.machineSelected] = Object.assign({}, machinesSelectorUpdated[state.machineSelected], { className: 'machine' });
+      machinesSelectorUpdated[selected] = Object.assign({}, machinesSelectorUpdated[selected], { className: 'selected' });
+      return Object.assign({}, state, { machinesSelector: machinesSelectorUpdated, machineSelected: selected });
     }
     default:
       return state;
