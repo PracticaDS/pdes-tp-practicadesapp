3df551e54747ad75e834ead66f876c7369173631
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-05-08 22:41:53 -0300

    next positions

--- src/reducers/panel.js
+++ src/reducers/panel.js
@@ -3,11 +3,21 @@ import { type as updateMachineSelected } from '../actions/updateMachineSelected'
 import machines from '../data/machines';
 import machinesSelector from '../data/machinesSelector';
 
+const getPositions = pos => {
+  const positions = [];
+  if(pos - 10 >= 0) positions.push(pos - 10);
+  if(pos + 10 <= 99) positions.push(pos + 10);
+  if(pos % 10 !== 0 && pos !== 0) positions.push(pos - 1);
+  if(pos + 1 % 10 !== 0 && pos !== 99) positions.push(pos + 1);
+  return positions;
+}
+
 const initialState = { 
   machines, 
   selected: 24, 
   machinesSelector, 
-  machineSelected: -1
+  machineSelected: -1,
+  rawMaterialPositions: []
 };
 
 function panel(state = initialState, { type, selected }) {
@@ -19,6 +29,11 @@ function panel(state = initialState, { type, selected }) {
         state.machineSelected === -1
         ? { className: 'selected' }
         : { className: 'selected', src: state.machinesSelector[state.machineSelected].src });
+      const { type, exitPosition } = state.machines[selected];
+      if(type === 'starter') {
+        const nextPositions = getPositions(selected);
+        // nextPositions.forEach(pos => state.machines[pos])
+      }
       return Object.assign({}, state, { machines: machinesUpdated, selected });
     }
     case updateMachineSelected: {
